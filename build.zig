24cbed641341ed8d92f1a766f79221d1c32c2f29
logandavies181 <logan.davies181@gmail.com>
2025-11-03 21:49:30 +1300

    update build for 0.15.1

--- build.zig
+++ build.zig
@@ -1,41 +1,28 @@
 const std = @import("std");
 const rlz = @import("raylib_zig");
 
-pub fn build(b: *std.Build) !void {
+pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
+    const exe = b.addExecutable(.{
+        .name = "conway",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = optimize,
+            .imports = &.{
+            },
+        }),
+    });
 
     const raylib_dep = b.dependency("raylib_zig", .{
         .target = target,
         .optimize = optimize,
-        .linux_display_backend = .X11,
     });
-
     const raylib = raylib_dep.module("raylib");
     const raylib_artifact = raylib_dep.artifact("raylib");
-
-    //web exports are completely separate
-    if (target.query.os_tag == .emscripten) {
-        const exe_lib = try rlz.emcc.compileForEmscripten(b, "conway", "src/main.zig", target, optimize);
-
-        exe_lib.linkLibrary(raylib_artifact);
-        exe_lib.root_module.addImport("raylib", raylib);
-
-        // Note that raylib itself is not actually added to the exe_lib output file, so it also needs to be linked with emscripten.
-        const link_step = try rlz.emcc.linkWithEmscripten(b, &[_]*std.Build.Step.Compile{ exe_lib, raylib_artifact });
-        //this lets your program access files like "resources/my-image.png":
-        link_step.addArg("--embed-file");
-        link_step.addArg("resources/");
-
-        b.getInstallStep().dependOn(&link_step.step);
-        const run_step = try rlz.emcc.emscriptenRunStep(b);
-        run_step.step.dependOn(&link_step.step);
-        const run_option = b.step("run", "Run conway");
-        run_option.dependOn(&run_step.step);
-        return;
-    }
-
-    const exe = b.addExecutable(.{ .name = "conway", .root_source_file = b.path("src/main.zig"), .optimize = optimize, .target = target });
+    exe.linkLibrary(raylib_artifact);
+    exe.root_module.addImport("raylib", raylib);
 
     const options = b.addOptions();
     const notDebug = optimize != .Debug;
@@ -48,12 +35,19 @@ pub fn build(b: *std.Build) !void {
         exe.entry = .{ .symbol_name = "mainCRTStartup" };
     }
 
-    exe.linkLibrary(raylib_artifact);
-    exe.root_module.addImport("raylib", raylib);
-
-    const run_cmd = b.addRunArtifact(exe);
-    const run_step = b.step("run", "Run conway");
-    run_step.dependOn(&run_cmd.step);
-
     b.installArtifact(exe);
+
+    const run_step = b.step("run", "Run the app");
+    const run_cmd = b.addRunArtifact(exe);
+    run_step.dependOn(&run_cmd.step);
+    run_cmd.step.dependOn(b.getInstallStep());
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+    const exe_tests = b.addTest(.{
+        .root_module = exe.root_module,
+    });
+    const run_exe_tests = b.addRunArtifact(exe_tests);
+    const test_step = b.step("test", "Run tests");
+    test_step.dependOn(&run_exe_tests.step);
 }
